0326ed31b96af68e26f8758b3907db353e830253
Folka2134 <folka2134@gmail.com>
2023-09-06 13:11:46 +0100

    created demo section with form and input

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Demo</h1>
-    </div>
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex w-full flex-col gap-2">
+        <form
+          className="relative flex items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+
+        {/* Previous searches */}
+      </div>
+
+      {/* Display results */}
+    </section>
   );
 };
 
