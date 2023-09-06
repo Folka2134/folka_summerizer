4a555c4a3ced528b9a30a53f0f11a14941cda11a
Folka2134 <folka2134@gmail.com>
2023-09-06 13:02:41 +0100

    updated hero component with navbar and titles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <header className="flex w-full flex-col items-center justify-center">
+      <nav className="mb-10 flex w-full items-center justify-between pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/folka2134")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GTP-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with summize and open-source article summarizer
+        that transforms lengthly articles into clear and concise summaries
+      </h2>
+    </header>
   );
 };
 
