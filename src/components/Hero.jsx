39b048ae4de43642b18752dc30d597fcd99ea449
Folka2134 <folka2134@gmail.com>
2023-09-07 10:21:43 +0100

    updated images and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,11 @@ const Hero = () => {
   return (
     <header className="flex w-full flex-col items-center justify-center">
       <nav className="mb-10 flex w-full items-center justify-between pt-3">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img
+          src={logo}
+          alt="logo"
+          className="w-28 rounded-full object-contain"
+        />
         <button
           type="button"
           onClick={() => window.open("https://github.com/folka2134")}
@@ -15,8 +19,8 @@ const Hero = () => {
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient">OpenAI GTP-4</span>
+        Summarise Articles with <br className="max-md:hidden" />
+        <span className="blue_gradient">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with summize and open-source article summarizer
